9104cdb8646ddbb05dfe0b41d2b67abb82f1d710
francois gourbal <gourbal.francois@gmail.com>
2022-10-11 11:49:15 +0200

    ajout de la lettre aléatoire, modification de la verification des lettres utilisées, ne marche pas entièrement

--- RequestUsr/AskLetter.go
+++ RequestUsr/AskLetter.go
@@ -2,18 +2,21 @@ package RequestUsr
 
 import (
 	"bufio"
+	"hangman-classic/Verify"
 	"os"
 )
 
-func AskLetter() string { // Fonction qui demande à l'utilisateur une lettre
+func AskLetter(ListLetterUsed []string) string { // Fonction qui demande à l'utilisateur une lettre
 	scanner := bufio.NewScanner(os.Stdin) // création du scanner capturant une entrée utilisateur
 	print("Choose: ")
 	scanner.Scan()           //lancement du scanner
-	letter := scanner.Text() // stockage du résultat du scanner dans une variable
-	if letter >= "A" && letter <= "Z" {
-		return letter
-	} else {
+	Letter := scanner.Text() // stockage du résultat du scanner dans une variable
+	if Letter < "A" || Letter > "Z" {
 		print("this character is not capital, this is not valid! \n")
 		return "0"
+	} else if Verify.VerifUsedLetter(Letter, ListLetterUsed) {
+		return "0"
+	} else {
+		return Letter
 	}
 }
