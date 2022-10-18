c3c55e9f1c66c5e71ae1762288602e26de805808
aromain <romain.auge@ynov.com>
2022-10-18 18:38:42 +0200

    test

--- RequestUsr/AskLetter.go
+++ RequestUsr/AskLetter.go
@@ -12,7 +12,7 @@ func AskLetter(ListLetterUsed []string) string { // Fonction qui demande à l'ut
 	scanner.Scan()           //lancement du scanner
 	Letter := scanner.Text() // stockage du résultat du scanner dans une variable
 	if Letter < "A" || Letter > "Z" {
-		print("this character  is not capital, this is not valid! \n")
+		print("this character is not capital, this is not valid! \n")
 		return "0"
 	} else if Verify.VerifUsedLetter(Letter, ListLetterUsed) {
 		return "0"
