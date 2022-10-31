fb3950b32423dd8ec0aea4addf80ceca7ff35435
rorolastico <romainauge31@gmail.com>
2022-10-31 16:45:59 +0100

    Correction du bug de fin de partie

--- Game/Game.go
+++ Game/Game.go
@@ -19,7 +19,7 @@ func Game(LineHangman int, ListLetterUsed []string, DashList []string, IndexOfDe
 	Print.DrawHangman(LineHangman, HangmanList)
 	Print.PrintDashList(DashList)
 	print("\n")
-	for IndexOfDeath < 9 {
+	for IndexOfDeath < 10 {
 		if Verify.VerifWon(DashList) == false {
 			letter := RequestUsr.AskLetter(ListLetterUsed, ListASCII, CharList)
 			ListLetterUsed = append(ListLetterUsed, letter)
@@ -38,41 +38,38 @@ func Game(LineHangman int, ListLetterUsed []string, DashList []string, IndexOfDe
 			} else {
 				CreateList.AddLettreInDashList(letter, DashList, IndexLetter)
 			}
-			NbDeathInString := strconv.FormatInt(int64(9-IndexOfDeath), 10)
-			message := "you have " + NbDeathInString + " attempts left"
+			NbDeathInString := strconv.FormatInt(int64(10-IndexOfDeath), 10)
+			message := "You have " + NbDeathInString + " attempts left"
 			ListWordASCII := CreateList.CreateASCIIWordList(message, ListASCII, CharList)
 			Print.PrintASCII(ListWordASCII)
-			//print("il vous reste, ", 9-IndexOfDeath, " essai.")
 			print("\n")
 			Print.DrawHangman(LineHangman, HangmanList)
 			print("\n")
 			fmt.Println("[", StringLetterUsed, "]")
 			if Verify.VerifOneTap(letter, ListWordCap) {
 				IndexOfDeath = 10
-				print(letter)
-				print("\n")
+				println(letter)
 				CreateList.AddLettreInDashList(letter, DashList, IndexLetter)
 				message := "GG, you're the best player !"
 				ListWordASCII := CreateList.CreateASCIIWordList(message, ListASCII, CharList)
 				Print.PrintASCII(ListWordASCII)
 			} else if Verify.VerifWon(DashList) {
+				IndexOfDeath = 10
 				CreateList.AddLettreInDashList(letter, DashList, IndexLetter)
 				Print.PrintDashList(DashList)
 				print("\n")
 				message := "GG, you win"
 				ListWordASCII := CreateList.CreateASCIIWordList(message, ListASCII, CharList)
 				Print.PrintASCII(ListWordASCII)
-				IndexOfDeath = 10
 			} else {
 				Print.PrintDashList(DashList)
 				print("\n")
 			}
-		} else {
-			IndexOfDeath = 10
-			message := "You Loose "
-			ListWordASCII := CreateList.CreateASCIIWordList(message, ListASCII, CharList)
-			Print.PrintASCII(ListWordASCII)
-			//print("You Loose")
 		}
+	}
+	if IndexOfDeath >= 9 && IndexOfDeath <= 10 {
+		message := "You Loose "
+		ListWordASCII := CreateList.CreateASCIIWordList(message, ListASCII, CharList)
+		Print.PrintASCII(ListWordASCII)
 	}
 }
