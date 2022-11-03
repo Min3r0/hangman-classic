05dedc5d85bb85510382d8ddffb078da55420ffb
GOURBAL François <gfrancoi@noreply.ytrack.learn.ynov.com>
2022-11-03 12:10:36 +0000

    Delete 'CreateList/CreateDashList.go'

--- CreateList/CreateDashList.go
+++
@@ -1,9 +0,0 @@
-package CreateList
-
-func CreateDashList(list []string) []string { // Fonction qui créer une liste de tiret aussi longue que le nombre de lettre du mot choisi
-	var DashListLetter []string
-	for dash := 0; dash < len(list); dash++ {
-		DashListLetter = append(DashListLetter, "_")
-	}
-	return DashListLetter
-}
