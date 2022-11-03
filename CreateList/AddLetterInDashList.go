15815df21b683d250e2251138bbd13438cb0b20c
GOURBAL François <gfrancoi@noreply.ytrack.learn.ynov.com>
2022-11-03 12:10:32 +0000

    Delete 'CreateList/AddLetterInDashList.go'

--- CreateList/AddLetterInDashList.go
+++
@@ -1,8 +0,0 @@
-package CreateList
-
-func AddLettreInDashList(lettre string, DashList []string, IndexLettre []int) []string {
-	for i := range IndexLettre {
-		DashList[IndexLettre[i]] = lettre
-	}
-	return DashList
-}
