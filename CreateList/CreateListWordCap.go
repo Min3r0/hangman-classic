a9454ad210ff6193713ebc4c210c946be7bb998b
GOURBAL François <gfrancoi@noreply.ytrack.learn.ynov.com>
2022-11-03 12:10:40 +0000

    Delete 'CreateList/CreateListWordCap.go'

--- CreateList/CreateListWordCap.go
+++
@@ -1,9 +0,0 @@
-package CreateList
-
-func CreateListWordCap(Word string) []string { //Fonction qui retourne une liste de lettre en majuscule
-	var ListLettre []string
-	for i := 0; i < len(Word); i++ {
-		ListLettre = append(ListLettre, string(Word[i]-32))
-	}
-	return ListLettre
-}
